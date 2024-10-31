ff535688c09d7055c038541bdc7b1a91e2824b21
duc-anh-2002 <nguyenducanh909.bkhn@gmail.com>
2024-10-31 07:00:55 +0000

    update sh

--- scripts/pretraining/wikitext-103/transformers/small/smoe-s.sh
+++ scripts/pretraining/wikitext-103/transformers/small/smoe-s.sh
@@ -1,7 +1,7 @@
 mkdir -p checkpoints/wikitext-103/transformers-s/smoe
 
 args="
---data /home/gtruong/Project/ICML2/data/wikitext-103 \
+--data /home/anhnd81/.cache/wikitext-103
 --base_arch transformer \
 --architecture sgsgsg \
 --gate_name smoe \
@@ -23,8 +23,9 @@ args="
 --checkpoint checkpoints/wikitext-103/transformers-s/smoe/smoe.pt \
 "
 
+#--data /home/ubuntu/workspace/dataset/wikitext-103 \
 echo "Training ..."
-python train.py $args
+python -m torch.distributed.launch --nproc_per_node=1 --master_port=1234 --use_env train.py $args
 
 echo "Evaluation ..."
 python train.py $args --full-eval-mode --batch-sz 8
